ff782ae82218528d485707b163339198cacd80f8
Kokora <henockkokora18@gmail.com>
2025-08-27 22:22:18 +0000

    commit

--- Backend/routes/categories.js
+++ Backend/routes/categories.js
@@ -3,7 +3,6 @@ const router = express.Router()
 const Category = require('../models/Category')
 const multer = require('multer');
 const cloudinary = require('../utils/cloudinary');
-const Category = require('../models/Category');
 
 const createSlug = (name) => name.toLowerCase().replace(/\s+/g, '-');
 
